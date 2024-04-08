728e551a39a45550eb62980b159dae35a091dcd8
Diego Ordaz Ríos <diegoor.ti22@utsjr.edu.mx>
2024-04-07 20:07:03 -0600

    di

--- src/components/NodeRedGraphs.js
+++ src/components/NodeRedGraphs.js
@@ -2,7 +2,7 @@
 const NodeRedGraphs = () => {
     return (
        <div className="iframeContainer">
-         <iframe src="http://192.168.0.11:1880/ui/#!/0?socketid=eL1PknotZGNpAjc2AAAB" width="100%" height="600px"></iframe>
+         <iframe src="http://localhost:1880/ui/" width="100%" height="600px"></iframe>
        </div>
     );
    };
